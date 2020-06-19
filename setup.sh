4a51a1b00bc273e5ce54cc92d60bdd44fc43fe8a
timovp <tmvp91@gmail.com>
2020-06-19 19:00:59 +0200

    added nginx files to copy later

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir ~/docker
+mkdir ~/docker/code-server-master
+mkdir ~/docker/code-server-user1
+mkdir ~/docker/code-server-user2
+mkdir ~/docker/nginx-proxy-manager
+cp -r nginx-proxy-manager/* ~/docker/nginx-proxy-manager/
