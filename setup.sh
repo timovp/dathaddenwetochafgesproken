3e3425cd42e202359b4c8b1ed9e5e97d00700a51
timovp <tmvp91@gmail.com>
2020-06-25 22:53:05 +0200

    same

--- setup.sh
+++ setup.sh
@@ -13,22 +13,22 @@ SUDOPASS=$(echo 'sudopass')
 # domain name, registered at digital ocean
 DOMAINNAME=$(echo 'dathaddenwetochafgesproken.nl')
 
-mkdir ~/docker
-mkdir ~/docker/code-server-master
-mkdir ~/docker/code-server-$USER1
-mkdir ~/docker/code-server-$USER2
-mkdir ~/docker/nginx-proxy-manager
-mkdir ~/docker/code-server-master/custom-init.d
-mkdir ~/docker/code-server-$USER1/custom-init.d
-mkdir ~/docker/code-server-$USER2/custom-init.d
+mkdir /root/docker
+mkdir /root/docker/code-server-master
+mkdir /root/docker/code-server-$USER1
+mkdir /root/docker/code-server-$USER2
+mkdir /root/docker/nginx-proxy-manager
+mkdir /root/docker/code-server-master/custom-init.d
+mkdir /root/docker/code-server-$USER1/custom-init.d
+mkdir /root/docker/code-server-$USER2/custom-init.d
 
-mkdir ~/docker/code-server-master/workspace/
-mkdir ~/docker/code-server-$USER1/workspace/
-mkdir ~/docker/code-server-$USER2/workspace/
+mkdir /root/docker/code-server-master/workspace/
+mkdir /root/docker/code-server-$USER1/workspace/
+mkdir /root/docker/code-server-$USER2/workspace/
 
-cp requirements.txt ~/docker/code-server-master/workspace/
-cp requirements.txt ~/docker/code-server-$USER1/workspace/
-cp requirements.txt ~/docker/code-server-$USER2/workspace/
+cp requirements.txt /root/docker/code-server-master/workspace/
+cp requirements.txt /root/docker/code-server-$USER1/workspace/
+cp requirements.txt /root/docker/code-server-$USER2/workspace/
 echo 'version: "2.1"
 services:
   code-server:
@@ -42,9 +42,9 @@ services:
       - SUDO_PASSWORD=master
       - PROXY_DOMAIN=vscode.'$DOMAINNAME' #optional
     volumes:
-      - ~/docker/code-server-master:/config
-      - ~/docker/code-server-'$USER1'/workspace:/config/workspace/'$USER1'
-      - ~/docker/code-server-'$USER2'/workspace:/config/workspace/'$USER2'
+      - /root/docker/code-server-master:/config
+      - /root/docker/code-server-'$USER1'/workspace:/config/workspace/'$USER1'
+      - /root/docker/code-server-'$USER2'/workspace:/config/workspace/'$USER2'
     ports:
       - 8443:8443
     restart: unless-stopped
@@ -60,7 +60,7 @@ services:
       - SUDO_PASSWORD='$SUDOPASS'
       - PROXY_DOMAIN='$USER1'.'$DOMAINNAME' #optional
     volumes:
-      - ~/docker/code-server-'$USER1':/config
+      - /root/docker/code-server-'$USER1':/config
     ports:
       - 8444:8443
     restart: unless-stopped
@@ -76,7 +76,7 @@ services:
       - SUDO_PASSWORD='$SUDOPASS'
       - PROXY_DOMAIN='$USER1'.'$DOMAINNAME' #optional
     volumes:
-      - ~/docker/code-server-'$USER2':/config
+      - /root/docker/code-server-'$USER2':/config
     ports:
       - 8445:8443
     restart: unless-stopped
@@ -89,15 +89,15 @@ services:
       - "80:8080"
       - "443:4443"
     volumes:
-      - "~/docker/nginx-proxy-manager:/config:rw"
-    restart: unless-stopped' > ~/docker/docker-compose.yml
-docker-compose -f ~/docker/docker-compose.yml up -d --remove-orphans --force-recreate
-cp install_python ~/docker/code-server-master/custom-init.d/
-cp install_python ~/docker/code-server-$USER1/custom-init.d/
-cp install_python ~/docker/code-server-$USER2/custom-init.d/
-docker-compose -f ~/docker/docker-compose.yml restart
+      - "/root/docker/nginx-proxy-manager:/config:rw"
+    restart: unless-stopped' > /root/docker/docker-compose.yml
+docker-compose -f /root/docker/docker-compose.yml up -d --remove-orphans --force-recreate
+cp install_python /root/docker/code-server-master/custom-init.d/
+cp install_python /root/docker/code-server-$USER1/custom-init.d/
+cp install_python /root/docker/code-server-$USER2/custom-init.d/
+docker-compose -f /root/docker/docker-compose.yml restart
 
-TOKEN=$(cat ~/token.txt)
+TOKEN=$(cat /root/token.txt)
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -d '{"type": "CNAME",
   "name": "'$USER1'",
   "data": "'$DOMAINNAME'.",
