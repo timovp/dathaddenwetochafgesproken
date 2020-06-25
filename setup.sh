167bf32272ba40f997939fc1636ed02c41608ab4
timovp <tmvp91@gmail.com>
2020-06-25 20:54:29 +0200

    added docker compose command

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ mkdir ~/docker/code-server-master
 mkdir ~/docker/code-server-user1
 mkdir ~/docker/code-server-user2
 mkdir ~/docker/nginx-proxy-manager
-cp -r nginx-proxy-manager/* ~/docker/nginx-proxy-manager/
+cp -r nginx-proxy-manager ./nginx-proxy-manager
+docker-compose up -d --remove-orphans --force-recreate
